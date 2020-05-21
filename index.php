d18fb6c7a347eb3b0d1f6dc68326b271debbcbac
memoparra <letme.grabthatface@gmail.com>
2020-05-21 02:18:56 -0400

    second commit

--- index.php
+++ index.php
@@ -8,8 +8,22 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <h1 style="text-align: center;">Hi !</h1>
+    <form class="main-container" name="calculator">
+        <h1 class="title">Calculator</h1>
+        <div class="body-calc">
+            <div class="screen">
+                <fieldset disabled>
+                    <input type="textfield" name="screen" value="">
+                </fieldset>
+            </div>
+            <div class="buttons btn-group">
+                <input type="button" value="9" onClick="document.calculator.screen.value+='9'">
+                <input type="button" value="8" onClick="document.calculator.screen.value+='8'">
+            </div>
+        </div>
+    </form>
 </body>
 </html>
